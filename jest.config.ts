edad6e4eae7b2610dcd82ba1a2c39a998fdc0938
Mendes Hugo <hugo.mendes@noreply.github.com>
2023-06-18 16:32:11 +0200

    fix(ci): get good files for code coverage

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import * as path from "path";
 import { JestConfigWithTsJest } from "ts-jest";
 
 export default {
-	collectCoverageFrom: ["<rootDir>/src/*.ts", "!<rootDir>/src/**/index.ts"],
+	collectCoverageFrom: ["<rootDir>/src/**/*.ts", "!<rootDir>/src/**/index.ts"],
 	coverageThreshold: { global: { branches: 75, functions: 75, lines: 75, statements: 75 } },
 	moduleFileExtensions: ["js", "ts"],
 	setupFilesAfterEnv: [path.resolve(__dirname, "./tools/jest/jest-extended.ts")],
